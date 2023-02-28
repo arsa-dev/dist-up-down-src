1f61bc23286ca9147edd5621a267e97252b707a6
Ryan Laws <ryanlaws@gmail.com>
2023-02-27 18:41:22 -0800

    Initialize repo

---
+++ README.md
@@ -0,0 +1,12 @@
+# Dist UP!
+
+F12 threw you down in a `/dist/` folder? Dist UP! will bring you back up where you wanna be!
+
+Use the Dist UP! command in the command palette. That's all there is to it.
+
+## Features
+
+- Opens the exact same file path without the last `/dist/`
+- Supposedly works in Windows (not tested)
+- Is simple
+- Has a short name
---
+++ src/extension.ts
@@ -0,0 +1,34 @@
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+	let distup = vscode.commands.registerCommand('distup.distup', () => {
+		if (vscode.window.activeTextEditor === undefined) {
+			vscode.window.showWarningMessage('No active editor - no dist to UP!')
+			return
+		} 
+
+		let { fileName } = vscode.window.activeTextEditor.document
+		if (fileName === 'untitled') {
+			vscode.window.showWarningMessage('Looks like a new file - no dist to UP!')
+			return
+		} 
+
+		fileName = fileName.split('\\').join('/') // MS Windows
+
+		const lastIndexOfDist = fileName.lastIndexOf('/dist/')
+		if (lastIndexOfDist === -1) {
+			vscode.window.showWarningMessage('Path has no /dist/ to UP!')
+			return
+		} 
+
+		const distUp = fileName.substring(0, lastIndexOfDist) + fileName.substring(lastIndexOfDist+5)
+		vscode.window.showInformationMessage(`DIST UP! ${distUp}`);
+		vscode.workspace.openTextDocument(vscode.Uri.file(distUp)).then(document => {
+			vscode.window.showTextDocument(document)
+		})
+	});
+
+	context.subscriptions.push(distup);
+}
+
+export function deactivate() {}
