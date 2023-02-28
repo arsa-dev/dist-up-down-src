67625e97a4f8aaa1032bbf2f3c38aaa9cfd54b56
Ryan Laws <ryanlaws@gmail.com>
2023-02-27 20:56:07 -0800

    Fix bug

--- src/extension.ts
+++ src/extension.ts
@@ -22,7 +22,7 @@ export function activate(context: vscode.ExtensionContext) {
 		} 
 
 		if (fileName.endsWith('.d.ts')) {
-			fileName = fileName.slice(fileName.length - ('.d.ts'.length)) + '.ts'
+			fileName = fileName.substring(0, fileName.length - ('.d.ts'.length)) + '.ts'
 		}
 
 		const distUp = fileName.substring(0, lastIndexOfDist) + fileName.substring(lastIndexOfDist+5)
